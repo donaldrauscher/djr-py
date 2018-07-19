096e3088813b7786d3d9ed4ecc5c8f6744154fec
Don Rauscher <donald.rauscher@mobeforlife.com>
2018-07-19 12:42:46 -0500

    Fix relative modules; pip --> pip3 in GCB step

---
+++ djr_py/skl/__init__.py
@@ -0,0 +1,13 @@
+from .hcc import SingleHCCEncoder
+from .hcc import HCCEncoder
+from .onehot import LabelEncoder
+from .onehot import OneHotEncoder
+from .stack import StackingClassifier
+
+__all__ = [
+    'SingleHCCEncoder',
+    'HCCEncoder',
+    'LabelEncoder',
+    'OneHotEncoder',
+    'StackingClassifier'
+]
