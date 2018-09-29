feed2dc75dbfa7b7b05ae1af7a9f57c998d065e2
Donald Rauscher <donald.rauscher@gmail.com>
2018-09-29 15:13:48 -0500

    Adjust dependencies

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ setup(
     author_email='donald.rauscher@gmail.com',
     license='MIT',
     install_requires=[
-        'numpy>=1.14.0,<1.15.0',    # per scikit-learn
-        'scipy>=1.1.0',             # per scikit-learn
-        'pandas>=0.23.3',           # most recent
-        'scikit-learn<0.20.0',      # most recent
-        'PyYAML>=3.13',             # most recent
-        'dill>=0.2.8.2'             # most recent
+        'numpy>=1.14.0',                    # per scikit-learn
+        'scipy>=1.1.0',                     # per scikit-learn
+        'pandas>=0.23.3',                   # most recent
+        'scikit-learn>=0.19.0,<0.20.0',     # most recent
+        'PyYAML>=3.13',                     # most recent
+        'dill>=0.2.8.2'                     # most recent
     ],
     extras_require={
         'tf': ['tensorflow>=1.11.0'],
